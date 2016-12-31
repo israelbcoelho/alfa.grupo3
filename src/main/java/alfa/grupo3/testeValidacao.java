a46db09df84f86571067ccbf4b00f75c0dd369ce
Israel Batista Coelho Junior <israel.junior@pcserver.pcinformatica.com.br>
2016-12-31 10:48:07 -0200

    teste

--- src/main/java/alfa/grupo3/testeValidacao.java
+++ src/main/java/alfa/grupo3/testeValidacao.java
@@ -5,7 +5,7 @@ public class testeValidacao {
 	public static void main(String[] args) {
 
 		validador validarCPF = new validador();
-		String CPF = "93801890101";
+		String CPF = "93801890104";
 		
 		   if (validarCPF.isCPF(CPF) == true)
 		       System.out.printf("%s\n", validarCPF.imprimeCPF(CPF));
